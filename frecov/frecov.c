bca827cc1207942d1d75d6449ac4664c5e503348
tracer-nju <tracer@nju.edu.cn>
2020-05-31 01:29:54 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  01:29:54 up 1 day, 13:05,  1 user,  load average: 1.30, 0.52, 0.38

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -379,7 +379,7 @@ void check_rgb(int width, int left ,void *p){
         memcpy(next_line_2, p + width - left, left);
         cnt += compare(prev_line_1 + left, next_line_1, width - left, 100);
         cnt += compare(prev_line_2, next_line_2, left, 100);
-        if(cnt <= width/8){
+        if(cnt <= width/2){
             free(prev_line_2);
             free(prev_line_1);
             free(next_line_2);
