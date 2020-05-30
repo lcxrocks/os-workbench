7d43a475743ec6c9f0832bc3e490999563cffd62
tracer-nju <tracer@nju.edu.cn>
2020-05-30 18:39:08 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  18:39:08 up 1 day,  6:17,  1 user,  load average: 0.45, 0.55, 0.53

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -159,11 +159,6 @@ void write_image(int fd, image_t * p);
 void get_line_rgb(int8_t *prev_line, int size, void *p);
 int compare(int8_t *prev_line , int8_t *next_line, int cnt);
 
-int unused_cnt;
-int bmp_data_cnt;
-int bmp_header_cnt;
-int empty_cnt;
-int long_name_cnt;
 int dirent_cnt;
 int pic_cnt;
 int nr_datasec;
@@ -234,28 +229,32 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+    printf("================================================================\n");
+    printf("dirent : %d\n", dirent_cnt);
+    printf("pic cnt: %d\n", pic_cnt);
+    printf("================================================================\n");
 
     //3. RECOVER IMAGES
-    image_t *p = &list_head;
-    while(p->next){
-        p = p->next;
-        char path_name[128] = "../../tmp/";
-        strcat(path_name, p->name);
-        int fd = open(path_name, O_CREAT | O_WRONLY, S_IRWXU);
-        //printf("ERROR: %d\n", errno);
-        panic_on(fd<0, "Bad fd");
-        //write(fd, p->bmp->header, p->size); // 连续的size大小
-        write_image(fd, p);
-        char sha1sum[256] = "sha1sum ";
-        strcat(sha1sum, path_name);
-        FILE *fp = popen(sha1sum, "r");
-        //panic_on(!fp, "popen");
-        char buf[256];
-        memset(buf, 0, sizeof(buf));
-        fscanf(fp, "%s", buf); // Get it!
-        pclose(fp);
-        printf("%s %s\n", buf, p->name);
-    }
+    // image_t *p = &list_head;
+    // while(p->next){
+    //     p = p->next;
+    //     char path_name[128] = "/tmp/";
+    //     strcat(path_name, p->name);
+    //     int fd = open(path_name, O_CREAT | O_WRONLY, S_IRWXU);
+    //     //printf("ERROR: %d\n", errno);
+    //     //panic_on(fd<0, "Bad fd");
+    //     //write(fd, p->bmp->header, p->size); // 连续的size大小
+    //     write_image(fd, p);
+    //     char sha1sum[256] = "sha1sum ";
+    //     strcat(sha1sum, path_name);
+    //     FILE *fp = popen(sha1sum, "r");
+    //     //panic_on(!fp, "popen");
+    //     char buf[256];
+    //     memset(buf, 0, sizeof(buf));
+    //     fscanf(fp, "%s", buf); // Get it!
+    //     pclose(fp);
+    //     printf("%s %s\n", buf, p->name);
+    // }
 }
 
 void dir_handler(void *c){
@@ -313,8 +312,8 @@ void dir_handler(void *c){
     int len = strlen(pic->name);
     
     if(pic->name[len-3] == 'b' && pic->name[len-2] == 'm'  && pic->name[len-1] == 'p' && pic->size != 0){
-       pic_cnt++;
-        printf("\033[32m>> dectected file name: \033[0m , clus_idx :%x, file_size: %d\n", pic->clus_idx, pic->size);
+        pic_cnt++;
+        printf("\033[32m>> dectected file name: \033[0m%s , clus_idx :%x, file_size: %d\n", pic->name, pic->clus_idx, pic->size);
     }
     else{
         list_head.next = pic->next;
