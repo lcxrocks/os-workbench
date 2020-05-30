344d296d11563f37c16bf68d8096f936e48e7577
tracer-nju <tracer@nju.edu.cn>
2020-05-31 01:34:03 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  01:34:03 up 1 day, 13:09,  1 user,  load average: 0.36, 0.50, 0.41

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -337,7 +337,7 @@ void dir_handler(void *c){
     int len = strlen(pic->name);
     if(pic->name[len-3] == 'b' && pic->name[len-2] == 'm'  && pic->name[len-1] == 'p' && pic->size != 0){
         pic_cnt++;
-        //printf("\033[32m>> dectected file name: \033[0m%s , clus_idx :%x, file_size: %d\n", pic->name, pic->clus_idx, pic->size);
+        printf("\033[32m>> dectected file name: \033[0m%s , clus_idx :%x, file_size: %d\n", pic->name, pic->clus_idx, pic->size);
     }
     else{
         list_head.next = pic->next;
