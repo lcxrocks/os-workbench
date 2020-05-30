abd2a22eb22bb65d5dcf623c2950d50864cc845f
tracer-nju <tracer@nju.edu.cn>
2020-05-30 14:51:27 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  14:51:27 up 1 day,  4:04,  1 user,  load average: 0.42, 0.49, 0.46

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -349,36 +349,36 @@ void write_image(int fd, image_t * ptr){
     //write(fd, ptr->bmp->header, ptr->size);
     void *p = ptr->bmp->header;
     void *t = disk->data;
-    write(fd, p, (ptr->size < BytsClus ? ptr->size : BytsClus));
-    write(fd, p, (ptr->size < BytsClus ? ptr->size : BytsClus));
-    write(fd, p, (ptr->size < BytsClus ? ptr->size : BytsClus));
+    
     write(fd, p, (ptr->size < BytsClus ? ptr->size : BytsClus)); num--; size -= BytsClus;// first cluster
     memcpy(prev_line, p+BytsClus-3*w, 3*w); 
     p += BytsClus;
     memcpy(next_line, p, 3*w);;
     
     int sum = 0;
-    // while(num > 0 && size > 0){
-    //     sum = compare(prev_line, next_line, 3*w);
-    //     while(sum > w * 3 * 20){// allow +-20 per digit per color 
-    //         t = t + BytsClus;//greedy_find_next_cluster();
-    //         memcpy(next_line, t, 3*w);
-    //         sum = compare(prev_line, next_line, 3*w);
-    //     }
-    //     if(t!=disk->data){
-    //         write(fd, t, size < BytsClus ? size : BytsClus);
-    //         memcpy(prev_line, t+BytsClus-3*w, 3*w);
-    //         num--; size -= BytsClus; 
-    //         t = disk->data;
-    //     }
-    //     else{
-    //         write(fd, p, size < BytsClus ? size : BytsClus);
-    //         memcpy(prev_line, p+BytsClus-3*w, 3*w);
-    //         num--; size -= BytsClus;
-    //     }
-    //     p += BytsClus;
-    //     memcpy(next_line, p, 3*w);
-    // }
+    while(num > 0 && size > 0){
+        sum = compare(prev_line, next_line, 3*w);
+        if(sum) printf(" ");
+        // while(sum > w * 3 * 20){// allow +-20 per digit per color 
+        //     t = t + BytsClus;//greedy_find_next_cluster();
+        //     memcpy(next_line, t, 3*w);
+        //     sum = compare(prev_line, next_line, 3*w);
+        // }
+        // if(t!=disk->data){
+        //     write(fd, t, size < BytsClus ? size : BytsClus);
+        //     memcpy(prev_line, t+BytsClus-3*w, 3*w);
+        //     num--; size -= BytsClus; 
+        //     t = disk->data;
+        // }
+        // else{
+        //     write(fd, p, size < BytsClus ? size : BytsClus);
+        //     memcpy(prev_line, p+BytsClus-3*w, 3*w);
+        //     num--; size -= BytsClus;
+        // }
+        num--;
+        p += BytsClus;
+        memcpy(next_line, p, 3*w);
+    }
 
 };
 
