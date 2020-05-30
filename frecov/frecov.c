45c8fc966ab89a1c604e3d7a130af868569ce129
tracer-nju <tracer@nju.edu.cn>
2020-05-30 15:55:00 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  15:55:00 up 1 day,  4:40,  1 user,  load average: 1.03, 0.64, 0.41

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -340,20 +340,18 @@ void write_image(int fd, image_t * ptr){
     int w = ptr->bmp->info->biWidth;
     int h = ptr->bmp->info->biHeight; // default: 24bit bmp file
 
-    int8_t  *prev_line = (int8_t *) malloc(2000);
-    int8_t  *next_line = (int8_t *) malloc(2000); // R G B
+    int8_t  *prev_line = (int8_t *) calloc(3*w, sizeof(int8_t));
+    int8_t  *next_line = (int8_t *) calloc(3*w, sizeof(int8_t)); // R G B
 
     int size = ptr->size;
     int num = size / BytsClus; // total number of clusters
     printf("\033[32m >>File: \033[0m \033[33m%s \033[0m\033[32mhas %d clusters to write.\033[0m\n", ptr->name, num);
-    //write(fd, ptr->bmp->header, ptr->size);
     void *p = ptr->bmp->header;
     void *t = disk->data;
     
     write(fd, p, (ptr->size < BytsClus ? ptr->size : BytsClus)); num--; size -= BytsClus;// first cluster
-    printf("wtf?\n");
+
     void *tmp = p+BytsClus-3*w;
-    printf("tmp : %p\n", tmp);
     for (int i = 0; i < 3*w; i++)
     {
         printf("ha\n");
