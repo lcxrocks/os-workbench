e9f54caab3bcd4703c671f6b866f9478ec563c76
tracer-nju <tracer@nju.edu.cn>
2020-05-30 18:48:44 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  18:48:44 up 1 day,  6:27,  1 user,  load average: 0.12, 0.22, 0.37

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -261,7 +261,7 @@ void dir_handler(void *c){
     image_t *p = malloc(sizeof(image_t));
     fat_dir *d = c;
     int num = 1; 
-    while(num==1 || ((d->LDIR_Ord & 0x40 )!= 0x40) ){
+    while(((d->LDIR_Ord & 0x40 )!= 0x40) ){
         d = (void *)d - 32;
         num++;
     }
