141437cbfbff8e5543e36bc00856a8c52fb86293
tracer-nju <tracer@nju.edu.cn>
2020-05-31 10:51:09 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  10:51:10 up 1 day, 17:34,  1 user,  load average: 0.24, 0.47, 0.51

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -409,7 +409,7 @@ void check_rgb(int width, int left ,void **p_t, int skip){
     cnt += compare(prev_line_1 + left, next_line_1, width - left -skip, 100);
     cnt += compare(prev_line_2, next_line_2, left, 100);
     
-    //if(cnt <= width/3){
+    if(cnt <= width/3){
         // printf("continus! ");
         // printf("cnt/width: %d/%d\n", cnt, width);
         free(prev_line_2);
@@ -419,9 +419,10 @@ void check_rgb(int width, int left ,void **p_t, int skip){
         *p_t = p;
         label[get_nclu(p)] = USED;
         return ;
-    //}
+    }
     
     p = disk->data;
+    int best_fit = width; //max
     for (void *ptr = disk->data; ptr < disk->end; ptr+=BytsClus)
     {
         cnt = 0;
@@ -431,21 +432,25 @@ void check_rgb(int width, int left ,void **p_t, int skip){
         cnt += compare(prev_line_1 + left, next_line_1, width - left - skip, 100);
         cnt += compare(prev_line_2, next_line_2, left, 100);
         
-        if(cnt <= width/8){ 
-            //  printf("not continus! ");
-            //  printf("cnt: %d \n", cnt);
-            free(prev_line_2);
-            free(prev_line_1);
-            free(next_line_2);
-            free(next_line_1);
-            p = ptr; *p_t = p;
-            label[get_nclu(p)] = USED;
-            printf("\033[31mptr at: %p, p should be: %p\033[0m\n", ptr, p);
-            // printf("disk->data:%p\n", disk->data);
-            return ;
+        if(cnt <= best_fit){
+            best_fit = cnt;
+            p = ptr;
         }
+        // if(cnt <= best_fit){ 
+        //     //  printf("not continus! ");
+        //     //  printf("cnt: %d \n", cnt);
+        //     free(prev_line_2);
+        //     free(prev_line_1);
+        //     free(next_line_2);
+        //     free(next_line_1);
+        //     p = ptr; *p_t = p;
+        //     label[get_nclu(p)] = USED;
+        //     printf("\033[31mptr at: %p, p should be: %p\033[0m\n", ptr, p);
+        //     // printf("disk->data:%p\n", disk->data);
+        //     return ;
+        // }
     }
-    *p_t = tmp;
+    *p_t = p+BytsClus > disk->end? tmp:p;
     label[get_nclu(p)] = USED;
     return ;
 }
