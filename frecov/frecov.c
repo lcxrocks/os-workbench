731c31c07ad400a73800fcdf7793412fcb183a5e
tracer-nju <tracer@nju.edu.cn>
2020-05-31 10:17:50 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  10:17:50 up 1 day, 17:00,  1 user,  load average: 0.82, 0.66, 0.56

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -409,7 +409,7 @@ int check_rgb(int width, int left ,void **p_t, int skip, int bound){
     cnt += compare(prev_line_1 + left, next_line_1, width - left -skip, 100);
     cnt += compare(prev_line_2, next_line_2, left, 100);
     
-    if(cnt <= 5*bound){
+    if(cnt <= 10*bound){
        printf("continus! ");
         printf("cnt/width: %d/%d\n", cnt, width);
         free(prev_line_2);
