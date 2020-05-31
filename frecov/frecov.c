b69a8f8888cfd38af764af38f64db59fd4dbabc4
tracer-nju <tracer@nju.edu.cn>
2020-05-31 08:31:54 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: a36cb5d218e94d1ca6a0dbce3ca520c5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  08:31:54 up 1 day, 15:15,  1 user,  load average: 0.24, 0.51, 0.56

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -408,7 +408,7 @@ void check_rgb(int width, int left ,void *p, int skip){
     cnt += compare(prev_line_1 + left, next_line_1, width - left -skip, 100);
     cnt += compare(prev_line_2, next_line_2, left, 100);
     
-    if(cnt <= width/2){
+    if(cnt <= width/4){
         printf("cnt/width: %d/%d\n", cnt, width);
         free(prev_line_2);
         free(prev_line_1);
@@ -427,7 +427,7 @@ void check_rgb(int width, int left ,void *p, int skip){
         cnt += compare(prev_line_1 + left, next_line_1, width - left - skip, 100);
         cnt += compare(prev_line_2, next_line_2, left, 100);
         
-        if(cnt <= width/8){printf("cnt: %d \n", cnt);
+        if(cnt <= width/8){//printf("cnt: %d \n", cnt);
             free(prev_line_2);
             free(prev_line_1);
             free(next_line_2);
