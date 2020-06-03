442fb4385c57f978bb746bfdf28a9b09fe6bd4e2
tracer-nju <tracer@nju.edu.cn>
2020-06-03 18:00:59 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 683fcf0725e6426c9e7441426bd78a32     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  18:00:59 up 1 day,  8:02,  1 user,  load average: 0.59, 0.57, 0.52

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -5,6 +5,30 @@
 #include <klib.h>
 #include <klib-macros.h>
 
+#define RED 31
+#define GREEN 32
+#define YELLOW 33
+
+#define r_panic_on(cond, s) \
+    c_panic_on(RED, cond, s);
+
+#define c_panic_on(color, cond, s) \
+do{ \
+    if(cond) {\
+        printf("\033[36m[cpu(%d)]:\033[0m");\
+        printf("\033[%dm", color); \
+        panic_on(cond, s); \
+        printf("\033[0m"); \
+    }\
+}while(0)
+
+#define c_log(color, ...) \
+    printf("\033[32m[cpu(%d)]:\033[0m");\
+    printf("\033[%dm", color); \
+    printf(__VA_ARGS__); \
+    printf("\033[0m");
+
+
 // xv6 checking double lock;
 // int holding (struct spinlock *lock){
 //   int r;
@@ -13,5 +37,32 @@
 //   popcli();
 //   return r;
 // }
+
+struct spinlock{
+  intptr_t locked;       // Is the lock held?
+  int intr;  // sti() cli() control.
+  // For debugging:
+  char *name;        // Name of lock.
+  int cpu;   // The cpu holding the lock.
+};
+
+struct semaphore{
+    int value;
+    struct spinlock lock;
+    // For debugging:
+    char *name;
+    int cpu;
+};
+
+struct task {
+  struct {
+    const char *name;
+    struct task *next;
+    _Context   *context;
+    //For debugging:
+    int cpu;
+  };
+  uint8_t stack[4096]; //4096-HDRsize
+};
 
 #endif
--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -1,26 +1,66 @@
 #include <common.h>
+//#include <kernel.h>
+//#include "../include/common.h"
+
+void kmt_spin_init(spinlock_t *lock, const char* name){
+   lock->name = (char *) name;
+   lock->locked = 0;  
+}
+
+void kmt_lock(spinlock_t *lock){
+    int i = _intr_read(); //cli();
+    _intr_write(0);
+    while(_atomic_xchg(&(lock->locked), 1) == 1) ;
+
+    // Get the lock successfully.
+    lock->intr = i;
+    lock->cpu = _cpu();
+    r_panic_on(_intr_read() != 0, "cli() failed in kmt_lock!\n");
+    r_panic_on(lock->locked != 1, "lock failed!\n");
+}
+
+void kmt_unlock(spinlock_t *lock){
+    _atomic_xchg(&(lock->locked), 0);
+    if(lock->intr) _intr_write(1);
+    r_panic_on(lock->locked != 0, "unlock failed!\n");
+    r_panic_on(lock->intr != _intr_read(), "sti() failed in kmt_unlock!\n");
+}
+
+void sem_init(sem_t *sem, const char *name, int value){
+    sem->name = (char *) name;
+    sem->value  = value;
+    kmt_spin_init(&(sem->lock), name);
+}
+
+void sem_wait(sem_t *sem){
+
+}
+
+void sem_signal(sem_t *sem){
+
+}
 
 void kmt_init(){
     return ;
-};
+}
 
 int  kcreate(task_t *task, const char *name, void (*entry)(void *arg), void *arg){
     return 0;
-};
+}
 
 void kteardown(task_t *task){
     return ;
+}
+
+
+MODULE_DEF(kmt) = {
+  .init  = kmt_init,
+  .create = kcreate,
+  .teardown  = kteardown,
+  .spin_init = kmt_spin_init,
+  .spin_lock = kmt_lock,
+  .spin_unlock = kmt_unlock,
+  .sem_init = sem_init, 
+  .sem_wait = sem_wait,
+  .sem_signal = sem_signal,
 };
-
-
-// MODULE_DEF(kmt) = {
-//   .init  = kmt_init,
-//   .create = kcreate,
-//   .teardown  = kteardown,
-//   .spin_init = spin_init,
-//   .spin_lock = lock,
-//   .spin_unlock = unlock,
-//   .sem_init = sem_init, 
-//   .sem_wait = sem_wait,
-//   .sem_signal = sem_signal,
-// };
