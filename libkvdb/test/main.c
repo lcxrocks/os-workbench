828cff21da01b6b73a34cd45ad6f969cb8fb61a9
tracer-nju <tracer@nju.edu.cn>
2020-06-10 22:48:14 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 683fcf0725e6426c9e7441426bd78a32     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  22:48:14 up 5 days, 15:14,  1 user,  load average: 0.48, 0.40, 0.43

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -12,20 +12,20 @@ int main() {
   //r_panic_on(sizeof(log_t)!=17*MB, "Wrong log header");
   //r_panic_on(sizeof(table_t)!=1*MB, "Wrong header sz");
   panic_on(!(db = kvdb_open("/tmp/c.db")), "cannot open db"); // 打开数据库
-  // for (int i = 0; i < 250; i++)
-  // {
-  //   char num[127];
-  //   sprintf(num, "%d", i);
-  //   kvdb_put(db, num, num);
-  // }
-  // c_log(GREEN, "Finished kvdb_put()!\n");
-  // for (int i = 0; i < 250; i++)
-  // {
-  //   char num[127];
-  //   sprintf(num, "%d", i);
-  //   value = kvdb_get(db, num);
-  //   printf("[%s]: [%s]\n", num, value);
-  // }
+  for (int i = 0; i < 250; i++)
+  {
+    char num[127];
+    sprintf(num, "%d", 250 - i);
+    kvdb_put(db, num, num);
+  }
+  c_log(GREEN, "Finished kvdb_put()!\n");
+  for (int i = 0; i < 250; i++)
+  {
+    char num[127];
+    sprintf(num, "%d", i);
+    value = kvdb_get(db, num);
+    printf("[%s]: [%s]\n", num, value);
+  }
 
   kvdb_put(db, "lcx", "good");
   kvdb_put(db, key, "three-easy-pieces"); // db[key] = "three-easy-pieces"
