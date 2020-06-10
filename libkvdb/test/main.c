ca4e82c63f98039cbae52b56e7b00024b2cfaabb
tracer-nju <tracer@nju.edu.cn>
2020-06-10 23:11:32 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 683fcf0725e6426c9e7441426bd78a32     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  23:11:32 up 5 days, 15:38,  1 user,  load average: 0.43, 0.48, 0.43

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -10,7 +10,7 @@ int main() {
   //r_panic_on(sizeof(log_t)!=17*MB, "Wrong log header");
   //r_panic_on(sizeof(table_t)!=1*MB, "Wrong header sz");
   panic_on(!(db = kvdb_open("/tmp/t.db")), "cannot open db"); // 打开数据库
-  kvdb_put(db, key, "three-easy-pieces"); // db[key] = "three-easy-pieces"
+  kvdb_put(db, key, "threeces"); // db[key] = "three-easy-pieces"
   value = kvdb_get(db, key); // value = db[key];
   kvdb_close(db); // 关闭数据库
   printf("[%s]: [%s]\n", key, value);
