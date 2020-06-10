f87ea2f854c2c9b1d0ff73ee0e6f4756ce6fce4d
tracer-nju <tracer@nju.edu.cn>
2020-06-10 22:26:19 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 683fcf0725e6426c9e7441426bd78a32     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  22:26:19 up 5 days, 14:52,  1 user,  load average: 0.46, 0.43, 0.40

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -167,8 +167,6 @@ void write_table_and_file(int fd, const char *key, const char *value){
       write_fd(fd, &Table, 17*MB, 1*MB); // write in table
       write_fd(fd, value, Table.start[key_id], strlen(value)+1);
       Log.commit = 0;
-      write_fd(fd, &Log, 0, sizeof(int));
-      fsync(fd);
     }
     else{
       Table.len[key_id] = Log.nr_block;
@@ -177,8 +175,6 @@ void write_table_and_file(int fd, const char *key, const char *value){
       write_fd(fd, &Table, 17*MB, 1*MB); // write in table
       write_fd(fd, value, RSVDSZ + (Table.block_cnt- Log.nr_block)*BLOCKSZ, strlen(value)+1);
       Log.commit = 0;
-      write_fd(fd, &Log, 0, sizeof(int));
-      fsync(fd);
     }
   }
   else{ // didn't find
@@ -194,14 +190,14 @@ void write_table_and_file(int fd, const char *key, const char *value){
     write_fd(fd, &Table, 17*MB, 1*MB); // write in table
     write_fd(fd, value, RSVDSZ + (Table.block_cnt- Log.nr_block)*BLOCKSZ, strlen(value)+1);
     Log.commit = 0;
-    write_fd(fd, &Log, 0, sizeof(int));
-    fsync(fd);
   }
 }
 
 void write_hdr(int fd, const char *key, const char *value){
   write_log(fd, key, value);
   write_table_and_file(fd, key, value);
+  write_fd(fd, &Log, 0, sizeof(int));
+  fsync(fd);
 }
 
 int kvdb_put(struct kvdb *db, const char *key, const char *value) {
