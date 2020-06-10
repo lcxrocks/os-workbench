642cd8bf48f75968ac63aa16737a4a66582dd546
tracer-nju <tracer@nju.edu.cn>
2020-06-10 22:47:01 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 683fcf0725e6426c9e7441426bd78a32     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  22:47:01 up 5 days, 15:13,  1 user,  load average: 0.58, 0.41, 0.44

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -31,9 +31,12 @@ int main() {
   kvdb_put(db, key, "three-easy-pieces"); // db[key] = "three-easy-pieces"
   kvdb_put(db, key, "three-hard-pieces");
   kvdb_put(db, "lcx", "very good");
+
   value = kvdb_get(db, key); // value = db[key];
   char *lcx = kvdb_get(db, "lcx");
   printf("[%s]: [%s]\n", "lcx", lcx);
+  char *one = kvdb_get(db, "1");
+  printf("[%s]: [%s]\n", "1", one);
   kvdb_close(db); // 关闭数据库
   printf("[%s]: [%s]\n", key, value);
   free(value);
