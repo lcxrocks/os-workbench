36c3f1ec8463b2b99bc6369127a171564a84b777
tracer-nju <tracer@nju.edu.cn>
2020-06-10 17:04:00 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 683fcf0725e6426c9e7441426bd78a32     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  17:04:00 up 5 days, 11:11,  1 user,  load average: 0.24, 0.28, 0.33

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -3,31 +3,6 @@
 log_t Log;
 table_t Table;
 
-void read_hdr(kvdb_t *db){
-  lseek(db->fd, 0, SEEK_SET);
-  read(db->fd, &Log, sizeof(log_t));
-  lseek(db->fd, 17*MB, SEEK_SET);
-  read(db->fd, &Table, sizeof(table_t));  
-}
-
-void fsck(kvdb_t *db){
-  read_hdr(db);
-  if(!Log.TxB){
-    return ;
-  }// No current log. proceed.
-  if(Log.TxB == 1 && Log.TxE == 1 && Log.commit == 1){
-    return ;
-  }// Log already commited. proceed.
-  if(Log.TxB == 1 && Log.TxE == 0){
-    return ;
-  }// Data lost. break when writing data. Usr to blame. proceed.
-  if(Log.TxB == 1 && Log.TxE == 1 && Log.commit == 0){
-    // REPAIR!!!
-  }// Log didn't commit. Maintenance needed.
-  c_log(GREEN, "+++ Doing fsck for : ");
-  c_log(CYAN, "[%s]\n", db->filename);
-}
-
 struct kvdb *kvdb_open(const char *filename) {
   kvdb_t *db = malloc(sizeof(kvdb_t));
   strcpy(db->filename, filename);
@@ -50,6 +25,39 @@ int kvdb_close(struct kvdb *db) {
   return 0;
 }
 
+void read_hdr(kvdb_t *db){
+  lseek(db->fd, 0, SEEK_SET);
+  read(db->fd, &Log, sizeof(log_t));
+  lseek(db->fd, 17*MB, SEEK_SET);
+  read(db->fd, &Table, sizeof(table_t));  
+}
+
+int find_key(const char *key){
+  for (int i = 0; i < Table.key_cnt; i++)
+  {
+    if(!strcmp(key, Table.key[i])) return i;
+  }
+  return -1;
+}// return key number 
+
+void fsck(kvdb_t *db){
+  read_hdr(db);
+  if(!Log.TxB){
+    return ;
+  }// No current log. proceed.
+  if(Log.TxB == 1 && Log.TxE == 1 && Log.commit == 1){
+    return ;
+  }// Log already commited. proceed.
+  if(Log.TxB == 1 && Log.TxE == 0){
+    return ;
+  }// Data lost. break when writing data. Usr to blame. proceed.
+  if(Log.TxB == 1 && Log.TxE == 1 && Log.commit == 0){
+    // REPAIR!!!
+  }// Log didn't commit. Maintenance needed.
+  c_log(GREEN, "+++ Doing fsck for : ");
+  c_log(CYAN, "[%s]\n", db->filename);
+}
+
 void write_log(int fd, const char *key, const char *value){
   int len = strlen(value);
   Log.TxB = 1;
@@ -62,14 +70,6 @@ void write_log(int fd, const char *key, const char *value){
   fsync(fd);
 }
 
-int find_key(const char *key){
-  for (int i = 0; i < Table.key_cnt; i++)
-  {
-    if(!strcmp(key, Table.key[i])) return i;
-  }
-  return -1;
-}// return key number 
-
 void write_fd(int fd, const void *buf, off_t offset, int len){
     lseek(fd, offset, SEEK_SET);
     write(fd, buf, len);
@@ -81,10 +81,25 @@ void write_hdr(int fd, const char *key, const char *value){
   int key_id = find_key(key);
   int len = strlen(value) + 1;
   if( key_id != -1){
-    //case 1: key.len > Log.nr_block
+    //case 1: key.len (old)> Log.nr_block(new)
     //case 2: key.len <= Log.nr_block
     printf("Already have this key!\n");
-    return ;
+    if(Log.nr_block <= Table.len[key_id]){
+      Table.len[key_id] = Log.nr_block;
+      write_fd(fd, &Table, 17*MB, 1*MB); // write in table
+      Log.TxE = 1;
+      write_fd(fd, value, Table.start[key_id], strlen(value)+1);
+      Log.commit = 1;
+    }
+    else{
+      Table.len[key_id] = Log.nr_block;
+      Table.start[key_id] = DATA_START + Table.block_cnt * BLOCKSZ;
+      Table.block_cnt += Log.nr_block;
+      write_fd(fd, &Table, 17*MB, 1*MB); // write in table
+      Log.TxE = 1;
+      write_fd(fd, value, RSVDSZ + (Table.block_cnt- Log.nr_block)*BLOCKSZ, strlen(value)+1);
+      Log.commit = 1;
+    } 
   }
   else{ // didn't find
     key_id = Table.key_cnt;
@@ -96,21 +111,18 @@ void write_hdr(int fd, const char *key, const char *value){
     Table.key_cnt++;
     Table.block_cnt += Log.nr_block;
     write_fd(fd, &Table, 17*MB, 1*MB); // write in table
+    Log.TxE = 1;
     write_fd(fd, value, RSVDSZ + (Table.block_cnt- Log.nr_block)*BLOCKSZ, strlen(value)+1);
+    Log.commit = 1;
   }
-  Log.TxE = 1;
-
-  write(fd, &Log, 17*MB);
-  write(fd, &Table, 1*MB);
+  write(fd, &Log, 4*sizeof(int));
   fsync(fd);
 }
 
 int kvdb_put(struct kvdb *db, const char *key, const char *value) {
   flock(db->fd, LOCK_EX);
   fsck(db);
-  // 1. log
   write_hdr(db->fd, key, value);
-  // 2. write in data
   flock(db->fd, LOCK_UN);
   return 0; 
 }
@@ -120,20 +132,21 @@ char *kvdb_get(struct kvdb *db, const char *key) {
   fsck(db);
   // read the data
   int key_id = find_key(key);
-  char *ret = malloc(4*MB);
-  if(key_id != -1){
-    printf("key: %d, start: %zx, len: %d\n", key_id, Table.start[key_id], Table.len[key_id]);
-    lseek(db->fd, Table.start[key_id], SEEK_SET);
-    int bytes= read(db->fd, ret, Table.len[key_id]*BLOCKSZ);
-    int length = printf("%s\n", ret);
-    printf("len: %d\n", length);
-    c_log(GREEN, "bytes : %d\n", bytes);
-  }
-  else{
+  
+
+  if(key_id == -1){
     c_log(RED, "didn't find key\n");
-    free(ret);
-    ret = NULL;
+    flock(db->fd, LOCK_UN);
+    return NULL;
   }
+  char *ret = malloc(Table.len[key_id] * BLOCKSZ);
+  printf("key: %d, start: %zx, len: %d\n", key_id, Table.start[key_id], Table.len[key_id]);
+  lseek(db->fd, Table.start[key_id], SEEK_SET);
+  int bytes= read(db->fd, ret, Table.len[key_id]*BLOCKSZ);
+  int length = printf("%s\n", ret);
+  printf("len: %d\n", length);
+  c_log(GREEN, "bytes : %d\n", bytes);
+  
   // char value[SUPER_BIG] = malloc(value);
   flock(db->fd, LOCK_UN);
   return ret;
