2fb88ac1545720982a0ed2b22ab0a8343130330e
tracer-nju <tracer@nju.edu.cn>
2020-06-10 16:21:44 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 683fcf0725e6426c9e7441426bd78a32     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  16:21:44 up 5 days, 10:29,  1 user,  load average: 0.84, 0.57, 0.52

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -132,7 +132,7 @@ char *kvdb_get(struct kvdb *db, const char *key) {
     char *p = ret;
     for (int i = 0; i < Table.len[key_id]; i++)
     {
-      printf("%zd", (int *) p);
+      printf("%ls", (int *) p);
       p++;
     }
     
