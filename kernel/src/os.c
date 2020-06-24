05e33661dfe4b6e9721d20358632bf4b0aef5193
tracer-nju <tracer@nju.edu.cn>
2020-06-24 22:01:59 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 0350059cf4414f018c27c0079e801d16     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  22:02:00 up 5 days, 17:39,  1 user,  load average: 0.48, 0.32, 0.13

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -50,6 +50,7 @@ _Context *os_trap(_Event ev, _Context *context){
     if (h->event == _EVENT_NULL || h->event == ev.event) {
       c_log(YELLOW, "Try calling handler for evnet no.%d\n", h->event);
       _Context *r = h->handler(ev, context);
+      c_log(YELLOW, "Returned from handler for event no.%d\n", h->event);
       panic_on(r && next, "returning multiple contexts");
       if (r) next = r;
     }
