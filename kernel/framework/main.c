403c1e2ec633ad52abe97bc26ab7ef70f349733d
tracer-nju <tracer@nju.edu.cn>
2020-07-01 18:12:26 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  18:12:26 up 3 days,  2:21,  1 user,  load average: 0.43, 0.49, 0.32

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,10 +1,15 @@
-#include <kernel.h>
+//#include <kernel.h>
 #include <klib.h>
+#include <common.h>
+int tid = 0;
+
+void printer()  { tid++; while (1) { printf("%d", tid); } }
 
 int main() {
   _ioe_init();
   _cte_init(os->trap);
   //_vme_init(pmm->alloc, pmm->free);
+  kmt->create(pmm->alloc(sizeof(task_t)), "printer", printer, NULL);
   os->init();
   _mpe_init(os->run); //call-user-entry(os->run)
   return 1;
