8c177fa2a102d719f086ee44c51edbc06582248f
tracer-nju <tracer@nju.edu.cn>
2020-07-01 18:13:07 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  18:13:08 up 3 days,  2:21,  1 user,  load average: 0.56, 0.52, 0.34

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -9,8 +9,8 @@ int main() {
   _ioe_init();
   _cte_init(os->trap);
   //_vme_init(pmm->alloc, pmm->free);
-  kmt->create(pmm->alloc(sizeof(task_t)), "printer", printer, NULL);
   os->init();
+  kmt->create(pmm->alloc(sizeof(task_t)), "printer", printer, NULL);
   _mpe_init(os->run); //call-user-entry(os->run)
   return 1;
 }
