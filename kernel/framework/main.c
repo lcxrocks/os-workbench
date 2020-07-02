6f62858432ec5d6d277800b5b1f81cd564ae19ad
tracer-nju <tracer@nju.edu.cn>
2020-07-02 18:40:45 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  18:40:46 up 3 days, 16:49,  1 user,  load average: 0.39, 0.40, 0.34

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -2,9 +2,16 @@
 #include <klib.h>
 #include <common.h>
 int tid = 0;
+sem_t empty;
+sem_t fill;
+#define P(s) kmt->sem_wait(s);
+#define V(s) kmt->sem_signal(s);
 
 void printer()  { tid++; while (1) { printf("%d", tid); } }
 void printer2()  {  while (1) { printf("."); } }
+void producer() { while (1) { P(&empty); _putc('('); V(&fill);  } }
+void consumer() { while (1) { P(&fill);  _putc(')'); V(&empty); } }
+
 
 int main() {
   printf("CPU reset.\n");
@@ -12,8 +19,10 @@ int main() {
   _cte_init(os->trap);
   //_vme_init(pmm->alloc, pmm->free);
   os->init();
-  kmt->create(pmm->alloc(sizeof(task_t)), "printer", printer, NULL);
-  kmt->create(pmm->alloc(sizeof(task_t)), "printer2", printer2, NULL);
+  kmt->sem_init(&empty, "empty", 1);
+  kmt->sem_init(&fill, "fill", 1);
+  kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
+  kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
   _mpe_init(os->run); //call-user-entry(os->run)
   return 1;
 }
