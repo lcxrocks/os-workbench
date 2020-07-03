bba778632194b1e81b77165c17e00edada9cb749
tracer-nju <tracer@nju.edu.cn>
2020-07-03 15:28:25 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  15:28:26 up 4 days,  1:39,  1 user,  load average: 0.52, 0.37, 0.29

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -33,6 +33,7 @@ void kmt_spin_init(spinlock_t *lock, const char* name){
 
 void kmt_lock(spinlock_t *lock){
     int i = _intr_read(); //cli();
+    printf("locking lock[%s]\n", lock->name);
     _intr_write(0);
     r_panic_on(holding(lock), "lock(%s) tried to acquire itself while holding.\n", lock->name);
     while(_atomic_xchg(&(lock->locked), 1)) ;
