d96d1a8bbb0b012cfa0d6ba7b4117bda10e302f3
tracer-nju <tracer@nju.edu.cn>
2020-07-04 01:39:47 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  01:39:47 up 4 days, 10:35,  1 user,  load average: 0.91, 0.54, 0.59

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -43,7 +43,7 @@ _Context *os_trap(_Event ev, _Context *context){
   panic_on(!next, "returning NULL context");
   //r_panic_on(!IN_RANGE((void *)next->rip, RANGE(0x100000, &_etext)), "Returned wrong rip(0x%x).\n", next->rip);
   _Context *ctx = pmm->alloc(sizeof(_Context));
-  memcpy(ctx, next, sizeof(_Context));
+  memcpy(ctx, next, sizeof(_Context)+1);
   kmt->spin_unlock(&task_lock);
   return ctx;
 };
