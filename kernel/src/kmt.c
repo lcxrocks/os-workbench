23ffe4a17d655580dcdf7ac7703f6019ace11fc6
tracer-nju <tracer@nju.edu.cn>
2020-07-04 14:14:09 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  14:14:10 up 4 days, 17:14,  1 user,  load average: 0.44, 0.42, 0.45

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -133,7 +133,7 @@ _Context *kmt_schedule(_Event ev, _Context *ctx){
            sleep = false; 
            break;
         }
-        //printf("[%s]: %d\n", t->name, t->stat);
+        printf("[%s]: %d\n", t->name, t->stat);
         t = t->next;
     }
     panic_on(sleep == true, "All task sleeping.\n");
