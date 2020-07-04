8be74b03b0605c0b10ed7b65558ef345fcd852e8
tracer-nju <tracer@nju.edu.cn>
2020-07-04 09:24:42 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  09:24:43 up 4 days, 13:43,  1 user,  load average: 0.92, 0.58, 0.55

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -165,10 +165,10 @@ _Context *kmt_schedule(_Event ev, _Context *ctx){
         current->cpu = (current->cpu + 1)%_ncpu(); // Round-robin to next cpu.
         task_t *rep = pmm->alloc(sizeof(task_t));
         memcpy(rep->stack, p->stack, sizeof(p->stack)); 
-        _Context *ctx = pmm->alloc(sizeof(_Context));
-        memcpy(ctx, next, sizeof(_Context));
-        ctx->rsp = p->context->rsp + (uint64_t)(rep->stack - p->stack);
-        next = ctx;
+        rep->context = pmm->alloc(sizeof(_Context));
+        memcpy(rep->context, p->context, sizeof(_Context));
+        rep->context->rsp = rep->context->rsp + (uint64_t)rep->stack - (uint64_t)p->stack;
+        next = rep->context;
     }
     else{
         current = IDLE;
