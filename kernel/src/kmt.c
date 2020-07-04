0812162e66b5e235bed5b22897928a030cb7b214
tracer-nju <tracer@nju.edu.cn>
2020-07-04 14:15:42 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  14:15:43 up 4 days, 17:15,  1 user,  load average: 0.63, 0.45, 0.45

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -125,7 +125,7 @@ _Context *kmt_schedule(_Event ev, _Context *ctx){
     _Context *next = NULL;
     c_log(BLUE, "IN handler kmt_schedule!\n");
     task_t *t = task_head.next;
-    //printf("--------------------------------------\n");
+    c_log(WHITE, "--------------------------------------\n");
     bool sleep = false;
     while(t){
         if(t->stat == SLEEPING) sleep = true;
@@ -133,11 +133,11 @@ _Context *kmt_schedule(_Event ev, _Context *ctx){
            sleep = false; 
            break;
         }
-        printf("[%s]: %d\n", t->name, t->stat);
+        c_log(WHITE, "[%s]: %d\n", t->name, t->stat);
         t = t->next;
     }
     panic_on(sleep == true, "All task sleeping.\n");
-    //printf("======================================\n");
+    c_log(WHITE, "======================================\n");
     task_t *p = task_head.next;
     while(p){
         if(p->stat == EMBRYO || p->stat == RUNNABLE || p->stat == ZOMBIE){
