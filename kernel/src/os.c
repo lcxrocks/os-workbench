13f297e491a01bb6990c213e770da66a4a800325
tracer-nju <tracer@nju.edu.cn>
2020-07-06 15:54:29 +0800

    Static hostname: chauncey          Icon name: computer-vm            Chassis: vm         Machine ID: 842f139c1c526d45bafd9f12e8b4c5f0            Boot ID: 4778101ea1604728a9e43a69276a14a5     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  15:54:29 up 5 days, 14:33,  1 user,  load average: 2.01, 1.88, 1.51

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -21,7 +21,7 @@ static void os_run() {
   c_log(PURPLE, "Hello world from CPU #%d\n", _cpu());
   _intr_write(1); //开中断（write(0)为关中断）
   while(1){
-    _yield();
+    //_yield();
     //assert(0);
   }
 }
